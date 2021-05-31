d41807029381f4c3e03b110eaf346bb28b83af1a
irfanullahjan <irfanullah.jan@outlook.com>
2021-05-31 17:42:09 +0500

    Moved styles to src/common

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "../styles/globals.css";
+import "../common/styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { Layout } from "../common/components/Layout";
