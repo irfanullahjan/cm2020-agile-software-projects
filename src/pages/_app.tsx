83a49ecf5b2189138eb2225d6095beb1de6feaee
irfanullahjan <irfanullah.jan@outlook.com>
2021-05-31 17:32:16 +0500

    - Added reactstrap, navbar, basic layout. - Eslint, prettier added, config to fix.

---
+++ src/pages/_app.tsx
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../styles/globals.css";
+
+import type { AppProps } from "next/app";
+import { Layout } from "../common/components/Layout";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  const a = 2;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+export default MyApp;
