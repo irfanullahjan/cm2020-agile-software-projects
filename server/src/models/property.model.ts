08ec175f91d8842f41cf98c65793332cdd4968c0
irfanullahjan <irfanullah.jan@outlook.com>
2021-06-05 16:36:16 +0500

    Minor types improvement

--- server/src/models/property.model.ts
+++ server/src/models/property.model.ts
@@ -2,6 +2,18 @@ import {Entity, model, property, hasOne, hasMany} from '@loopback/repository';
 import {Address} from './address.model';
 import {Image} from './image.model';
 
+enum Type {
+  LAND = 'land',
+  HOUSE = 'house',
+  APARTMENT = 'apartment',
+  COMMERCIAL = 'commercial'
+}
+
+enum Offer {
+  SALE = 'sale',
+  RENT = 'rent',
+}
+
 @model()
 export class Property extends Entity {
   @property({
@@ -24,16 +36,22 @@ export class Property extends Entity {
   area: number;
 
   @property({
-    type: 'object',
+    type: 'string',
     required: true,
+    jsonSchema: {
+      enum: Object.values(Type)
+    }
   })
-  type: object;
+  type: Type;
 
   @property({
-    type: 'object',
+    type: 'string',
     required: true,
+    jsonSchema: {
+      enum: Object.values(Offer)
+    }
   })
-  offer: object;
+  offer: Offer;
 
   @property({
     type: 'date',
