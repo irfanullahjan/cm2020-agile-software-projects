36a7003f50b858ff0b7789c18ef8cd1dc61ffafb
Adele Gikonyo <53218392+adelewg@users.noreply.github.com>
2021-08-07 19:45:20 +0300

    updated 404 page styling

--- client/src/pages/404.tsx
+++ client/src/pages/404.tsx
@@ -2,11 +2,17 @@ import Link from 'next/link';
 
 const NotFound = () => {
     return (
-        <div className="not-found">
-            <h1>Oops ....</h1>
-            <h2>That page cannot be found.</h2>
-            <p>Go back to the <Link href='/'><a>Homepage</a>  </Link> </p>
-        </div>
+        <>
+            <div className="not-found">
+                <h1>404</h1>
+                <hr />
+                <h2>Page not found</h2>
+                <Link href='/'><a>Homepage</a></Link>
+            </div>
+            
+            
+        </>
+        
         
     )
 }
