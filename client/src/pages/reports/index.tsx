7ad4e1883ffb446472f10414d3e4c6668152f93f
Irfanullah Jan <irfanullah.jan@outlook.com>
2021-09-01 20:15:08 +0500

    improvements to reports page, useswr added

--- client/src/pages/reports/index.tsx
+++ client/src/pages/reports/index.tsx
@@ -3,50 +3,66 @@ import { useContext, useEffect, useState } from 'react';
 import { Table } from 'reactstrap';
 import { Spinner } from 'components/lib/Spinner';
 import Link from 'next/link';
+import useSwr from 'swr';
+import { fetcher } from 'utils/methods';
+import Error from 'next/error';
 
 export default function ReportedProperties() {
   const { user } = useContext(SessionContext);
-  const [reportedProperties, setReportedProperties] = useState<any[]>();
-  useEffect(() => {
-    fetch('/api/reported-properties')
-      .then(res => res.json())
-      .then(json =>
-        setReportedProperties(
-          json.sort((a: any, b: any) => b.reports.length - a.reports.length),
-        ),
-      );
-  }, []);
-  if (user?.realm !== 'admin') return <p>Unauthorized!!</p>;
-  if (!reportedProperties) return <Spinner />;
+  const { data: reportedProperties, error } = useSwr(
+    '/api/reported-properties',
+    fetcher,
+  );
+
+  if (user?.realm !== 'admin')
+    return (
+      <Error
+        statusCode={401}
+        title="Sorry! You are not authorized to view this page."
+      />
+    );
+
+  reportedProperties?.sort(
+    (a: any, b: any) => b.reports.length - a.reports.length,
+  );
+
+  if (error)
+    return <Error statusCode={error.status} title="Error fetching data" />;
+
   return (
     <>
       <h1>Reports</h1>
-      <Table>
-        <thead>
-          <tr>
-            <td>Title</td>
-            <td>Type</td>
-            <td>Offer</td>
-            <td>Reports</td>
-            <td></td>
-          </tr>
-        </thead>
-        <tbody>
-          {reportedProperties.map((property, i) => (
-            <tr key={i}>
-              <td>
-                <Link href={`/reports/${property.id}`}>{property.title}</Link>
-              </td>
-              <td>{property.type}</td>
-              <td>{property.offer}</td>
-              <td>{property.reports.length}</td>
-              <td>
-                <Link href={`/reports/${property.id}`}>View</Link>
-              </td>
+      {!reportedProperties && <Spinner />}
+      {reportedProperties?.length > 0 ? (
+        <Table>
+          <thead>
+            <tr>
+              <td>Title</td>
+              <td>Type</td>
+              <td>Offer</td>
+              <td>Reports</td>
+              <td></td>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {reportedProperties.map((property: any, i: number) => (
+              <tr key={i}>
+                <td>
+                  <Link href={`/reports/${property.id}`}>{property.title}</Link>
+                </td>
+                <td>{property.type}</td>
+                <td>{property.offer}</td>
+                <td>{property.reports.length}</td>
+                <td>
+                  <Link href={`/reports/${property.id}`}>View</Link>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ) : (
+        <p>No reports.</p>
+      )}
     </>
   );
 }
