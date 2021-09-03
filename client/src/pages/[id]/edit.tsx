f9086d4f656c51ca8c8bbf78588659cd84719d90
Irfanullah Jan <irfanullah.jan@outlook.com>
2021-09-03 21:06:37 +0500

    authorization error for new and edit properties

--- client/src/pages/[id]/edit.tsx
+++ client/src/pages/[id]/edit.tsx
@@ -2,10 +2,17 @@ import { useRouter } from 'next/dist/client/router';
 import { Spinner } from 'components/lib/Spinner';
 import { getAsString } from 'utils/getAsString';
 import { PropertyForm } from 'components/PropertyForm';
+import { useContext } from 'react';
+import { SessionContext } from '../../pages/_app';
+import Error from 'next/dist/pages/_error';
 
 export default function EditProperty() {
+  const { user } = useContext(SessionContext);
   const router = useRouter();
   const { id } = router.query;
+
+  if (!user)
+    return <Error statusCode={401} title="Please login to edit properties." />;
 
   if (!id) return <Spinner />;
   return (
--- client/src/pages/add.tsx
+++ client/src/pages/add.tsx
@@ -1,10 +1,16 @@
 import { PropertyForm } from 'components/PropertyForm';
 import { SessionContext } from '../pages/_app';
 import { useContext } from 'react';
+import Error from 'next/dist/pages/_error';
 
 export default function AddProperty() {
   const { user } = useContext(SessionContext);
-  if (!user) return <p>Unauthorized!!</p>;
+
+  if (!user)
+    return (
+      <Error statusCode={401} title="Please login to post a new property." />
+    );
+
   return (
     <>
       <h1>Add property</h1>
